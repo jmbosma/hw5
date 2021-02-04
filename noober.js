bfbcb616d0783f32ffc55dff8da94843b2820c3a
Jared Bosma <jared.bosma@kellogg.northwestern.edu>
2021-02-04 00:12:01 -0600

    commit

--- noober.js
+++ noober.js
@@ -67,7 +67,219 @@ function renderRides(ridesArray) {
   }
 }
 
+
+
+
 window.addEventListener('DOMContentLoaded', function() {
   // YOUR CODE
+  
+
+
+  // let AllButtons = document.querySelectorAll('.filter-button')
+  let allRidesButton = document.querySelector('#all-filter')
+  let NooberPurpleButton = document.querySelector('#noober-purple-filter')
+  let NooberPoolButton = document.querySelector('#noober-pool-filter')
+  let NooberXLButton = document.querySelector('#noober-xl-filter')
+  let NooberXButton = document.querySelector('#noober-x-filter')
+
+  console.log(allRidesButton.value)
+
+
+
+  let AllButtons = [allRidesButton, NooberPurpleButton, NooberPoolButton, NooberXLButton, NooberXButton]
+  // console.log(AllButtons[1])
+  
+
+    // event when the All Ride button is clicked
+    allRidesButton.addEventListener('click', async function(event){ 
+      event.preventDefault()
+
+      // remove backgorund color of any previously clicked noober list
+      allRidesButton.classList.remove('bg-gray-300')
+      NooberPurpleButton.classList.remove('bg-gray-300')
+      NooberPoolButton.classList.remove('bg-gray-300')
+      NooberXLButton.classList.remove('bg-gray-300')
+      NooberXButton.classList.remove('bg-gray-300')
+
+      // shade in the button of the noober class picked 
+      allRidesButton.classList.add('bg-gray-300')
+
+      // output to user in console that they clicked All Rides button 
+      console.log('The All Rides button was clicked')
+      let output = document.querySelector('.rides')
+      output.innerHTML = ''
+      // output.insertAdjacentHTML('beforeend', '<p> The All Rides button was clicked!</p>')
+      
+      // request the data from our "API"
+      let url = 'https://kiei451.com/api/rides.json'
+      let response = await fetch(url)
+      let json = await response.json()
+
+      //pass the array of rides to the provided renderRides() function 
+      renderRides(json)
+
+    })
+  
+
+    // event when the Noober Purple button is clicked
+    NooberPurpleButton.addEventListener('click', async function(event){ 
+      event.preventDefault()
+
+      // remove backgorund color of any previously clicked noober list
+      allRidesButton.classList.remove('bg-gray-300')
+      NooberPurpleButton.classList.remove('bg-gray-300')
+      NooberPoolButton.classList.remove('bg-gray-300')
+      NooberXLButton.classList.remove('bg-gray-300')
+      NooberXButton.classList.remove('bg-gray-300')
+
+      // shade in the button of the noober class picked 
+      NooberPurpleButton.classList.add('bg-gray-300')
+
+      // output to user in console that they clicked All Rides button 
+      console.log('The Noober Purple button was clicked')
+      let output = document.querySelector('.rides')
+      output.innerHTML = ''
+      // output.insertAdjacentHTML('beforeend', '<p> The Noober Purple button was clicked!</p>')
+      
+      // request the data from our "API"
+      let url = 'https://kiei451.com/api/rides.json'
+      let response = await fetch(url)
+      let json = await response.json()
+      const newArray = new Array()
+
+        for(let i = 0; i < json.length; i++) {  
+          
+          let ride = json[i]
+          let ServiceLevel = levelOfService(ride)
+
+          if (ServiceLevel == "Noober Purple"){
+            newArray.push(ride)
+          } 
+        }
+      
+      //pass the new array of rides to the provided renderRides() function 
+      renderRides(newArray)  
+   })  
+
+    // event when the Noober Pool button is clicked
+    NooberPoolButton.addEventListener('click', async function(event){ 
+      event.preventDefault()
+
+      // remove backgorund color of any previously clicked noober list
+      allRidesButton.classList.remove('bg-gray-300')
+      NooberPurpleButton.classList.remove('bg-gray-300')
+      NooberPoolButton.classList.remove('bg-gray-300')
+      NooberXLButton.classList.remove('bg-gray-300')
+      NooberXButton.classList.remove('bg-gray-300')
+
+      // shade in the button of the noober class picked 
+      NooberPoolButton.classList.add('bg-gray-300')
+
+      // output to user in console that they clicked All Rides button 
+      console.log('The Noober Pool button was clicked')      
+      let output = document.querySelector('.rides')
+      output.innerHTML = ''
+      // output.insertAdjacentHTML('beforeend', '<p> The Noober Pool button was clicked!</p>')
+      
+      // request the data from our "API"
+      let url = 'https://kiei451.com/api/rides.json'
+      let response = await fetch(url)
+      let json = await response.json()
+      const newArray = new Array()
+
+        for(let i = 0; i < json.length; i++) {  
+          
+          let ride = json[i]
+          let ServiceLevel = levelOfService(ride)
+
+          if (ServiceLevel == "Noober Pool"){
+            newArray.push(ride)
+          } 
+        }
+      
+      //pass the new array of rides to the provided renderRides() function 
+      renderRides(newArray)  
+    }) 
+    
+    // event when the Noober XL button is clicked
+    NooberXLButton.addEventListener('click', async function(event){ 
+      event.preventDefault()
+
+      // remove backgorund color of any previously clicked noober list
+      allRidesButton.classList.remove('bg-gray-300')
+      NooberPurpleButton.classList.remove('bg-gray-300')
+      NooberPoolButton.classList.remove('bg-gray-300')
+      NooberXLButton.classList.remove('bg-gray-300')
+      NooberXButton.classList.remove('bg-gray-300')
+
+      // shade in the button of the noober class picked 
+      NooberXLButton.classList.add('bg-gray-300')
+
+      // output to user in console that they clicked All Rides button 
+      console.log('The Noober XL button was clicked')
+      let output = document.querySelector('.rides')
+      output.innerHTML = ''
+      // output.insertAdjacentHTML('beforeend', '<p> The Noober XL button was clicked!</p>')
+      
+      // request the data from our "API"
+      let url = 'https://kiei451.com/api/rides.json'
+      let response = await fetch(url)
+      let json = await response.json()
+      const newArray = new Array()
+
+        for(let i = 0; i < json.length; i++) {  
+          
+          let ride = json[i]
+          let ServiceLevel = levelOfService(ride)
+
+          if (ServiceLevel == "Noober XL"){
+            newArray.push(ride)
+          } 
+        }
+      
+      //pass the new array of rides to the provided renderRides() function 
+      renderRides(newArray)  
+   })  
+
+    // event when the Noober X button is clicked
+    NooberXButton.addEventListener('click', async function(event){ 
+      event.preventDefault()
+
+      // remove backgorund color of any previously clicked noober list
+      allRidesButton.classList.remove('bg-gray-300')
+      NooberPurpleButton.classList.remove('bg-gray-300')
+      NooberPoolButton.classList.remove('bg-gray-300')
+      NooberXLButton.classList.remove('bg-gray-300')
+      NooberXButton.classList.remove('bg-gray-300')
+
+      // shade in the button of the noober class picked 
+      NooberXButton.classList.add('bg-gray-300')
+
+      // output to user in console that they clicked All Rides button 
+      console.log('The Noober X button was clicked')
+      let output = document.querySelector('.rides')
+      output.innerHTML = ''
+      // output.insertAdjacentHTML('beforeend', '<p> The Noober X button was clicked!</p>')
+      
+      // request the data from our "API"
+      let url = 'https://kiei451.com/api/rides.json'
+      let response = await fetch(url)
+      let json = await response.json()
+      const newArray = new Array()
+
+        for(let i = 0; i < json.length; i++) {  
+          
+          let ride = json[i]
+          let ServiceLevel = levelOfService(ride)
+
+          if (ServiceLevel == "Noober X"){
+            newArray.push(ride)
+          } 
+        }
+      
+      //pass the new array of rides to the provided renderRides() function 
+      renderRides(newArray)  
+   })   
+
 })
 
